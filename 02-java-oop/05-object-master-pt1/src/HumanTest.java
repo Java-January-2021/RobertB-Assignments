7c531df5f94d85e851e5cc8fbf948ff3fe6db6c6
Robert Buckley <rob.buckley93@gmail.com>
2021-01-20 14:57:13 -0600

    pt1 homework

---
+++ 02-java-oop/05-object-master-pt1/src/HumanTest.java
@@ -0,0 +1,13 @@
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Human jake = new Human();
+		Human rob = new Human();
+		
+		rob.attack(jake);
+		System.out.println(jake.getHealth());
+	}
+
+}
